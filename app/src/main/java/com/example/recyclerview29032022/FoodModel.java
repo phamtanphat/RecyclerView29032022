b73b5e76e73990c43567ba16fd24a4f49f19052e
Phat <phatdroid94@gmail.com>
2022-05-14 19:12:05 +0700

    model

--- app/src/main/java/com/example/recyclerview29032022/FoodModel.java
+++ app/src/main/java/com/example/recyclerview29032022/FoodModel.java
@@ -1,8 +1,90 @@
 package com.example.recyclerview29032022;
 
+import java.util.Date;
+import java.util.List;
+
 public class FoodModel {
     private int image;
     private String name;
     private String address;
+    private List<Category> categoryList;
+    private String discount;
+    private float distance;
+    private Date openTime;
+    private Date closeTime;
 
+    public FoodModel(int image, String name, String address, List<Category> categoryList, String discount, float distance, Date openTime, Date closeTime) {
+        this.image = image;
+        this.name = name;
+        this.address = address;
+        this.categoryList = categoryList;
+        this.discount = discount;
+        this.distance = distance;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public Date getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(Date openTime) {
+        this.openTime = openTime;
+    }
+
+    public Date getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(Date closeTime) {
+        this.closeTime = closeTime;
+    }
 }
--- app/src/main/java/com/example/recyclerview29032022/MainActivity.java
+++ app/src/main/java/com/example/recyclerview29032022/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("BBB",Category.BIRTHDAY.getText());
     }
 }
