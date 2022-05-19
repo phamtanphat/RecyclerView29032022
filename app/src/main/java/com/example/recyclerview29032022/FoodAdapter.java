329797abdaa75676a07bf893095992c9e0bd7c1e
Phat <phatdroid94@gmail.com>
2022-05-19 18:26:31 +0700

    handle data

--- app/src/main/java/com/example/recyclerview29032022/FoodAdapter.java
+++ app/src/main/java/com/example/recyclerview29032022/FoodAdapter.java
@@ -1,17 +1,53 @@
 package com.example.recyclerview29032022;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FoodAdapter {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
+
+    List<FoodModel> listFoods;
+
+    public FoodAdapter(List<FoodModel> listFoods) {
+        this.listFoods = listFoods;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.layout_item_food, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        FoodModel foodModel = listFoods.get(position);
+        holder.bind(foodModel);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (listFoods == null || listFoods.size() == 0) {
+            return 0;
+        }
+        return listFoods.size();
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder {
         ImageView img;
         TextView txtTimeOpen, txtName, txtAddress, txtCategory, txtDiscount, txtDistance;
+        StringBuilder textCategory;
+        SimpleDateFormat simpleDateFormat;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -23,6 +59,38 @@ public class FoodAdapter {
             txtDiscount = itemView.findViewById(R.id.textViewDiscount);
             txtDistance = itemView.findViewById(R.id.textViewDistance);
 
+        }
+
+        public void bind(FoodModel foodModel) {
+            img.setImageResource(foodModel.getImage());
+            txtName.setText(foodModel.getName());
+            txtAddress.setText(foodModel.getAddress());
+            txtDistance.setText(String.format(">%f", foodModel.getDistance()));
+            if (foodModel.getCategoryList().size() > 0) {
+                textCategory = new StringBuilder();
+                for (int i = 0; i < foodModel.getCategoryList().size(); i++) {
+                    if (i == foodModel.getCategoryList().size() - 1) {
+                        textCategory.append(foodModel.getCategoryList().get(i).getText()).append(" - ");
+                    } else {
+                        textCategory.append(foodModel.getCategoryList().get(i).getText());
+                    }
+                }
+            }
+            if (!foodModel.getDiscount().isEmpty()) {
+                txtDiscount.setVisibility(View.VISIBLE);
+                txtDiscount.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_new, 0, 0, 0);
+                txtDiscount.setText(foodModel.getDiscount());
+            } else {
+                txtDiscount.setVisibility(View.GONE);
+            }
+            Calendar calendar = Calendar.getInstance();
+            if (calendar.before(foodModel.getOpenTime()) || calendar.after(foodModel.getCloseTime())) {
+                txtTimeOpen.setVisibility(View.VISIBLE);
+                simpleDateFormat = new SimpleDateFormat("HH:mm");
+                txtTimeOpen.setText(String.format("Đóng cửa\nMở cửa vào lúc %s", simpleDateFormat.format(foodModel.getOpenTime())));
+            }else{
+                txtTimeOpen.setVisibility(View.GONE);
+            }
         }
     }
 }
