03c42d4424f973bdccd56d2df2d4580230aac347
Phat <phatdroid94@gmail.com>
2022-05-21 19:07:02 +0700

    layout item

--- app/src/main/java/com/example/recyclerview29032022/MainActivity.java
+++ app/src/main/java/com/example/recyclerview29032022/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.recyclerview29032022;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,16 +12,30 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView rcvFood;
     List<FoodModel> listFoods;
     FoodAdapter foodAdapter;
+    int totalPage, totalItem, currentPage, currentItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        totalItem = totalPage = currentItem = currentPage = 0;
         rcvFood = findViewById(R.id.recyclerView);
         listFoods = FoodModel.getMock();
         foodAdapter = new FoodAdapter(listFoods);
         rcvFood.setAdapter(foodAdapter);
         rcvFood.setHasFixedSize(true);
 
+        eventLoadMore();
     }
+
+    private void eventLoadMore() {
+        totalItem = 100;
+        totalPage = totalItem / 10;
+        currentItem = listFoods.size();
+        currentPage = totalPage - ((totalItem - currentItem) / 10);
+
+
+    }
+
+
 }
